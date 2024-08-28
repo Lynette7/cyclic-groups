53d76d98a96b63f01f889d0202bfd46151d95f8b
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-08-28 13:38:20 +0300

    chore: clean code and add comments

--- is_cyclic_multiplicative/src/main.rs
+++ is_cyclic_multiplicative/src/main.rs
@@ -1,3 +1,10 @@
+// The logical steps for this code are:
+// 1. Find the order of the group using φ(m) - find_order(m: u128)
+// 2. Find the members of the multiplicative group i.e integers that are relatively prime to the mod - multiplicative_group(modulus)
+// 3. Determine if the group is cyclic by generating the subgroups for each element in the group mod m and comparing it to the group - is_cyclic(m)
+// ** The is_cyclic function uses generate_group(g, order, m) to generate the groups for each element
+
+// Function to find the gcd of two integers recursively
 fn gcd(a: u128, b: u128) -> u128 {
     if b == 0 {
         a
@@ -6,6 +13,7 @@ fn gcd(a: u128, b: u128) -> u128 {
     }
 }
 
+// Function to determine if an integer is prime
 fn is_prime(n: u128) -> bool {
     if n <= 1 {
         return false;
@@ -18,6 +26,7 @@ fn is_prime(n: u128) -> bool {
     true
 }
 
+// Function to find the prime factors of an integer
 fn prime_factors(mut num: u128) -> Vec<u128> {
     let mut p = 2;
     let mut factors: Vec<u128> = Vec::new();
@@ -40,10 +49,10 @@ fn prime_factors(mut num: u128) -> Vec<u128> {
     return factors;
 }
 
+// Function to find the order of a group using Euler Phi
 fn find_order(m: u128) -> u128 {
     let factors: Vec<u128> = prime_factors(m);
     let prime = is_prime(m);
-    // let mut order = 0;
 
     if prime {
         let order = m - 1;
@@ -54,13 +63,12 @@ fn find_order(m: u128) -> u128 {
         for &factor in &factors {
             phi = phi * (factor - 1) / factor;
         }
-        let order = phi; // The order is φ(m)
+        let order = phi;
         return order;
     }
-
-    // order
 }
 
+// Generate the group of the Z mod m passed in main
 fn multiplicative_group(modulus: u128) -> Vec<u128> {
     let mut group = Vec::new();
     for j in 1..modulus {
@@ -72,15 +80,7 @@ fn multiplicative_group(modulus: u128) -> Vec<u128> {
     group
 }
 
-// fn additive_group(modulus: u128) -> Vec<u128> {
-//     let mut group = Vec::new();
-//     for i in 0..modulus {
-//         group.push(i);
-//     }
-
-//     group
-// }
-
+// Generate the subgroups for each element in the group
 fn generate_group(g: u128, order: u128, m: u128) -> Vec<u128> {
     let mut set: Vec<u128> = Vec::new();
     for j in 1..order+1 {
@@ -91,6 +91,7 @@ fn generate_group(g: u128, order: u128, m: u128) -> Vec<u128> {
     set
 }
 
+// Determine if the multiplicative group is cyclic
 fn is_cyclic(m: u128) -> bool {
     let order = find_order(m);
     println!("The order of this multiplicative group is {}", order);
