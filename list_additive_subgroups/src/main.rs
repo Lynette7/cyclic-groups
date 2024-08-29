97a57c87281f1d4f4c3b7bcb5f098ebdd46e6ef6
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-08-29 11:33:25 +0300

    chore: find the subgroups of an additive group

---
+++ list_additive_subgroups/src/main.rs
@@ -0,0 +1,40 @@
+use std::collections::HashSet;
+
+// Function to generate the subgroup of element g
+fn generate_subgroup(g: u32, m: u32) -> HashSet<u32> {
+    let mut subgroup = HashSet::new();
+    let mut element = 1;
+    let mut i = 0;
+
+    while element != 0 {
+        element = (g + i) % m;
+        subgroup.insert(element);
+        i += g;
+    }
+    subgroup
+}
+
+// Function to find all subgroups of the group
+fn list_all_subgroups(m: u32) -> Vec<HashSet<u32>> {
+    let mut subgroups = Vec::new();
+
+    for g in 1..=m {
+        if m % g == 0{
+            let subgroup = generate_subgroup(g, m);
+            if !subgroups.contains(&subgroup) {
+                subgroups.push(subgroup);
+            }
+        }
+    }
+    subgroups
+}
+
+fn main() {
+    let modulus = 12;
+    let subgroups = list_all_subgroups(modulus);
+
+    println!("Subgroups of the additive group Z{} are:", modulus);
+    for subgroup in subgroups {
+        println!("{:?}", subgroup);
+    }
+}
